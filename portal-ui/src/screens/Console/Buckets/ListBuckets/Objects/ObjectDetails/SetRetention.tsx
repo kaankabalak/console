4500dbe473492b76e72371681789c618b6da755d
Kaan Kabalak <kaan@minio.io>
2020-10-23 06:55:42 -0700

    Add Type field and buttons to Set Retention Policy modal

--- portal-ui/src/screens/Console/Buckets/ListBuckets/Objects/ObjectDetails/SetRetention.tsx
+++ portal-ui/src/screens/Console/Buckets/ListBuckets/Objects/ObjectDetails/SetRetention.tsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { createStyles, Theme, withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import { modalBasic } from "../../../../Common/FormComponents/common/styleLibrary";
 import ModalWrapper from "../../../../Common/ModalWrapper/ModalWrapper";
 import FormSwitchWrapper from "../../../../Common/FormComponents/FormSwitchWrapper/FormSwitchWrapper";
+import RadioGroupSelector from "../../../../Common/FormComponents/RadioGroupSelector/RadioGroupSelector";
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -11,6 +14,10 @@ const styles = (theme: Theme) =>
       fontWeight: 700,
       marginBottom: 40,
     },
+    buttonContainer: {
+      textAlign: "right",
+    },
+    ...modalBasic,
   });
 
 interface ISetRetentionProps {
@@ -27,10 +34,16 @@ const SetRetention = ({
   objectName,
 }: ISetRetentionProps) => {
   const [statusEnabled, setStatusEnabled] = useState<boolean>(false);
+  const [type, setType] = useState<string>("");
 
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     console.log("on submit");
+  };
+
+  const resetForm = () => {
+    setStatusEnabled(false);
+    setType("");
   };
 
   return (
@@ -57,12 +70,42 @@ const SetRetention = ({
             id="status"
             name="status"
             checked={statusEnabled}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
               setStatusEnabled(!statusEnabled);
+              setType("");
             }}
             label={"Status"}
             indicatorLabels={["Enabled", "Disabled"]}
           />
+        </Grid>
+        <Grid xs={12}>
+          <RadioGroupSelector
+            currentSelection={type}
+            id="type"
+            name="type"
+            label="Type"
+            disableOptions={!statusEnabled}
+            onChange={(e) => {
+              setType(e.target.value);
+            }}
+            selectorOptions={[
+              { label: "Governance", value: "governance" },
+              { label: "Compliance", value: "compliance" },
+            ]}
+          />
+        </Grid>
+        <Grid item xs={12} className={classes.buttonContainer}>
+          <button
+            type="button"
+            color="primary"
+            className={classes.clearButton}
+            onClick={resetForm}
+          >
+            Reset
+          </button>
+          <Button type="submit" variant="contained" color="primary">
+            Save
+          </Button>
         </Grid>
       </form>
     </ModalWrapper>
